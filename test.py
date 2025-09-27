e445acfc01d42c592b4b657286375e0795b052ff
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2025-09-27 11:47:44 +0300

    test on fashoin

--- test.py
+++ test.py
@@ -4,21 +4,28 @@ from database import load_mnist
 import matplotlib.pyplot as plt
 
 model = AutoEncoder()
-model.load_state_dict(torch.load("autoencoder.pth"))  
+model.load_state_dict(torch.load("autoencoder.pth"))
 model.eval()
 
-_, test_loader = load_mnist(batch_size=1)
+_, test_loader = load_mnist(batch_size=8)
 
 for x, _ in test_loader:
     output = model(x)
 
-    plt.subplot(1, 2, 1)
-    plt.title("Original")
-    plt.imshow(x[0][0], cmap='gray')
-
-    plt.subplot(1, 2, 2)
-    plt.title("Reconstructed")
-    plt.imshow(output[0][0].detach(), cmap='gray')
-
+    n = x.size(0)
+    
+    plt.figure(figsize=(16, 4))
+    
+    for i in range(n):
+        plt.subplot(2, n, i+1)
+        plt.title("Original")
+        plt.imshow(x[i][0], cmap='gray')
+        plt.axis('off')
+        
+        plt.subplot(2, n, n+i+1)
+        plt.title("Reconstructed")
+        plt.imshow(output[i][0].detach(), cmap='gray')
+        plt.axis('off')
+    
     plt.show()
     break  
